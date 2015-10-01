389408a7d431bcd07181b7e747504799bd4979d5
Phil Marshall <dr.phil.marshall@gmail.com>
2015-10-01 12:58:30 -0400

    Fixed PGM bugs

--- inference/straightline_utils.py
+++ inference/straightline_utils.py
@@ -6,8 +6,6 @@ import numpy as np
 # pylab: matplotlib's matlab-like interface
 import pylab as plt
 
-import daft
-
 # The data we will fit, sometimes:
 #  x, y, sigma_y
 data1 = np.array([[201,592,61],[244,401,25],[47,583,38],[287,402,15],[203,495,21],
@@ -133,37 +131,3 @@ def plot_line(m, b, **kwargs):
     plt.xlim(*xlimits)
     plt.ylim(*ylimits)
     return p
-
-
-def draw_pgm():
-
-    # Instantiate a PGM.
-    pgm = daft.PGM([3.3, 3.0], origin=[0.3, 0.3], grid_unit=2.6, node_unit=1.3, observed_style="inner")
-
-    # Model parameters:
-    pgm.add_node(daft.Node("m", r"$m$", 0.8, 2.8))
-    pgm.add_node(daft.Node("b", r"$b$", 1.8, 2.8))
-
-    # Latent variable - intrinsic y:
-    pgm.add_node(daft.Node("ytrue", r"$y^{\rm true}_k$", 1.3, 1.4, fixed=True, offset=(0,-25)))
-
-    # Data - observed magnitude:
-    pgm.add_node(daft.Node("y", r"$y_k$", 2.5, 1.4, observed=True))
-
-    # Constants - magnitude errors:
-    pgm.add_node(daft.Node("sigma", r"$\sigma_k$", 1.9, 0.9, fixed=True, offset=(-3,0)))
-
-    # Add in the edges.
-    pgm.add_edge("m", "ytrue")
-    pgm.add_edge("b", "ytrue")
-    pgm.add_edge("sigma", "y")
-    pgm.add_edge("ytrue", "y")
-
-    # And a plate for the pixels
-    pgm.add_plate(daft.Plate([0.5, 0.7, 2.7, 1.4], label=r"datapoints $k$", shift=-0.1))
-
-    # Render and save.
-    pgm.render()
-    pgm.figure.savefig("straightline_pgm.png", dpi=300)
-
-    return
